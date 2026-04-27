c755e71759492528436b8a0f57853a83fd628cb1
Vishvas0419 <vishvas0419@gmail.com>
2026-04-28 01:23:55 +0530

    Update Career Compass

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,17 +1,28 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
+// const pool = mysql.createPool({
+//   host: process.env.DB_HOST || 'localhost',
+//   user: process.env.DB_USER || 'root',
+//   password: process.env.DB_PASS || '',
+//   database: process.env.DB_NAME || 'careercompass',
+//   port: process.env.DB_PORT || 3306,
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0
+// });
+
+// const promisePool = pool.promise();
+// module.exports = promisePool;
+
+// const mysql = require("mysql2");
+
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'careercompass',
-  port: process.env.DB_PORT || 3306,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
 });
 
-const promisePool = pool.promise();
-module.exports = promisePool;
-
+module.exports = pool.promise();
